85fd2aee74ec652cd9e953736f677278d50efc9e
B V Satyaram <bvsatyaram@gmail.com>
2015-12-05 11:49:34 +0530

    Needee submission successful

--- app/controllers/needees_controller.rb
+++ app/controllers/needees_controller.rb
@@ -3,4 +3,21 @@ class NeedeesController < ApplicationController
     @needee = Needee.new
     @localities = Locality.all
   end
+
+  def create
+    @needee = Needee.new(needee_params)
+    if verify_recaptcha(model: @needee) && @needee.save
+      flash[:notice] = "A volunteer will contact you soon"
+      redirect_to root_path
+    else
+      @localities = Locality.all
+      flash[:alert] = "Please enter all details"
+      render :new
+    end
+  end
+
+private
+  def needee_params
+    params.require(:needee).permit(:name, :phone, :locality_id, :type_of_help, :address, :details)
+  end
 end
