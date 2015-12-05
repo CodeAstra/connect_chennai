bc5a876b992fa1c85d66f703a6076f614dec0d4b
B V Satyaram <bvsatyaram@gmail.com>
2015-12-05 17:49:09 +0530

    Fix GET path for signout in mobile phones

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   devise_scope :user do
-    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+    delete 'sign_out', :to => 'devise/sessions#destroy'
+    # Temp Fix: Mobile phone is posting GET request for signout.
+    get 'sign_out', :to => 'devise/sessions#destroy'
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
